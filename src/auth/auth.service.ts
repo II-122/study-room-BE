973bb1046081b50c336e9f762c5289714ab5382a
II-122 <76612998+II-122@users.noreply.github.com>
2024-10-14 21:32:18 +0900

    test2
    t2

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -73,7 +73,7 @@ export class AuthService {
 
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
-      secure: this.configService.get<string>('NODE_ENV') === 'production',
+      secure: true,
       sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
